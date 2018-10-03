1c1d75d9f1645e581faa0e57d3b4a0392188ab2d
bmartin97 <barsimartin@gmail.com>
2018-10-03 12:09:14 +0200

    added basic framework
    might not be the final framework, just something to get us started

---
+++ connect4/connect4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace connect4
+{
+    class Program
+    {
+
+        void Boolean winLose()
+        {
+            //win/lose check goes here
+        }
+
+        void playerTurn(int playerNum)
+        {
+            //player's turn goes here
+            int[] coordinates = {0, 0};
+            
+            placeXO(coordinates);
+        }
+
+        void placeXO(int[] coordinates)
+        {
+            //putting an X or O in the board wherever the parameter coordinates say 
+        }
+
+        void play()
+        {
+            //game goes here
+            playerTurn();
+        }
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
