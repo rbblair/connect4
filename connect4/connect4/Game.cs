f82d88d1261bea47446aa79fbd4d8aa657d0f64e
Rayne Blair <raynebblair@gmail.com>
2018-10-16 22:54:37 +0200

    added a bunch of functions for playing

--- connect4/connect4/Game.cs
+++ connect4/connect4/Game.cs
@@ -6,24 +6,25 @@ using System.Threading.Tasks;
 
 namespace connect4
 {
-    public class Game
+	public struct PlayerInfo
 	{
 		public String playerName;
 		public char playerID;
 	}
 
+	public class Game
 
     {
-		playerInfo playerOne = new playerInfo();
-		playerInfo playerTwo = new playerInfo();
+		//playerInfo playerOne = new playerInfo();
+		//playerInfo playerTwo = new playerInfo();
 
 		public static char[,] Grid = new char[13, 13] 
 			{ 
-				//Rows/Columns 1, 3, 5, 7, 9, and 11 have the empty slots for player X/O ([1,1], [1,3], [1,5] etc)
-				{'-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-'},
-				{'|', ' ', '|', ' ', '|', ' ', '|', ' ', '|', ' ', '|', ' ', '|'},
-				{'-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-'},
-				{'|', ' ', '|', ' ', '|', ' ', '|', ' ', '|', ' ', '|', ' ', '|'},
+				//Rows/Columns 1, 3, 5, 7, 9, and 11 have the empty slots for player X/O)
+				{'-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-'}, //Row 0 [0, x] [row, colum]
+				{'|', ' ', '|', ' ', '|', ' ', '|', ' ', '|', ' ', '|', ' ', '|'}, //Row 1 [1, x]
+				{'-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-'}, //Row 2 [2, x]
+				{'|', ' ', '|', ' ', '|', ' ', '|', ' ', '|', ' ', '|', ' ', '|'}, //etc
 				{'-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-'},
 				{'|', ' ', '|', ' ', '|', ' ', '|', ' ', '|', ' ', '|', ' ', '|'},
 				{'-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-'},
@@ -35,33 +36,54 @@ namespace connect4
 				{'-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-'}
 			};
 
-		public void instructions()
-		public void Instructions()
+		public static void Instructions()
 		{
 			Console.WriteLine("CONNECT FOUR");
 			Console.WriteLine("To play Connect 4, you must drop a token (X or O depending on player) into the empty slots.");
 			Console.WriteLine("Whoever gets 4 in a row vertically, horizontally, or diagonally wins.");
-			Console.WriteLine("Ready to play? Y/N");
-			
-
+			Console.WriteLine("Press any key to continue.");
+			Console.ReadKey();
 		}
 
-		public void placeXO(char XO,int colomn)
+		public static void placeXO(char XO,int column)
         {
-            //char would be altered in the main program depending on the player
-            //so after one plays there would be char variable x=X; and later x=O;
-            //we choose the colomn and while the items on that colomn are -1 we keep decsending otherwise
-            //we put X or O in the previous cell (carefull with grid[0,0],grid[1,0],grid[2,0]...grid[6,0]) can make
-            //out of range exception so should be checked seperately.
-            //-1 for empty 1 for X and 0 for O;
-            throw new NotImplementedException();
+			//char would be altered in the main program depending on the player
+			//so after one plays there would be char variable x=X; and later x=O;
+			//we choose the colomn and while the items on that colomn are -1 we keep decsending otherwise
+			//we put X or O in the previous cell (carefull with grid[0,0],grid[1,0],grid[2,0]...grid[6,0]) can make
+			//out of range exception so should be checked seperately.
+			//-1 for empty 1 for X and 0 for O;
 
+			for (int i = 12; i >= 0; i--)
+			{
+				if (i % 2 != 0)
+				{
+					if (Game.Grid[i, column] != ' ')
+					{
+						Game.Grid[i, column] = XO;
+					}
+				}
+			}
 
         }
 
-		public void IsThereWinner(out char winner)
+		static void PlayerTurn(int playerNum, char XO)
+		{
+			int choice = 0;
+			PrintBoard(Game.Grid);
+			Console.WriteLine("Player " + playerNum + "'s turn!");
+			Console.WriteLine("Which column would you like to put your token in?");
+			while (choice < 7 && choice > 0)
+			{
+				choice = int.Parse(Console.ReadLine());
+			}
+			placeXO(XO, choice);
+			Console.Clear();
+			PrintBoard(Game.Grid);
 
-		static void IsthereWinner(char[,] board, playerInfo activePlayer, int dropChoice)
+		}
+
+		static void IsthereWinner(char[,] board, PlayerInfo activePlayer, int dropChoice)
 		{
 			//need to check on lines colomns and diagonals
 			// lines should be (for i=0 to 1)
@@ -80,7 +102,6 @@ namespace connect4
 			//'N';
 			//should be the same for colomns just swap i and j!
 			//should check the same for diagonals(carefull with the range!)
-			throw new NotImplementedException();
 			do
 			{
 				if (board[length, dropChoice] != 'X' && board[length, dropChoice] != 'O')
@@ -133,20 +154,30 @@ namespace connect4
 
 			int rows = 13, columns = 13, x, y;
 			Console.WriteLine("CONNECT FOUR \n");
-			for (x = 0; x < columns; x++)
+			for (y = 0; y < columns; y++)
 			{ 
-				for (y = 0; y < rows; y++)
+				for (x = 0; x < rows; x++)
 				{
-					Console.Write(grid[x, y]);
+					Console.Write(grid[y, x]);
 				}
 				Console.WriteLine();
 			}
 		}
 
+		public static void PlayGame()
+		{
+			while (true)
+			{
+				PlayerTurn(1, 'O');
+				PlayerTurn(2, 'X');
+			}
+			
+		}
+
 		static void Main(string[] args)
 		{
 			Instructions();
-			PrintBoard(Game.Grid);
+			PlayGame();
 			Console.ReadKey();
 		}
 	}
